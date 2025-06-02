eecfa4647cbd1210a27e0d3e39dd1b9d05332e65
Vikash21198 <kumarvikash538@yahoo.com>
2025-06-02 23:07:50 +0530

    Add files via upload

---
+++ Simple_program.py
@@ -0,0 +1,17 @@
+A=input('First Number: ')
+B=input('Second Number: ')
+A=float(A)
+B=float(B)
+Addition=(A+B)
+print('Addition: ', Addition)
+Substraction=(A-B)
+print('Substraction: ', Substraction)
+Multiplication=(A*B)
+print('Multiplication: ', Multiplication)
+Division=(A/B)
+print('Division: ', Division)
+
+first_name=input('Enter your First Name: ')
+last_name=input('Enter your Second Name: ')
+full_name = first_name + " " + last_name
+print(f"Hi, {full_name}! Welocome to the python program.")
